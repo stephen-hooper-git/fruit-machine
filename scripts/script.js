e1ed286209bf151dfd5fc0e3446f5e146f7ac47f
stephen-hooper-git <stephen.hooper@outlook.com>
2021-07-16 21:29:10 +0100

    Reels innerHTML

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let fruits = '🍇🍈🍉🍊🍋🍌🍍🥭🍎🍏🍐🍑🍒🍓'
+
+let reel1 = document.getElementById('reel1')
+let reel2 = document.getElementById('reel2')
+let reel3 = document.getElementById('reel3')
+
+reel1.innerHTML = fruits
+reel2.innerHTML = fruits
+reel3.innerHTML = fruits
